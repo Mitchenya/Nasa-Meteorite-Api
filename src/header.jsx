d7290e8f069638271f713b34855fe45027c0179e
Mitchenya <Mitchellrjshepherd@outlook.com>
2024-09-10 23:41:20 +0100

    This commit with remove public from image file path

--- src/header.jsx
+++ src/header.jsx
@@ -3,11 +3,7 @@ const Header = () => {
     <div className="header">
       <h1>Destinations Impacted by Meteorites Over The Years.</h1>
       <div className="logo">
-        <img
-          src="public/meteor_logo_white.png"
-          alt="logo"
-          className="logo-size"
-        />
+        <img src="meteor_logo_white.png" alt="logo" className="logo-size" />
       </div>
     </div>
   );
