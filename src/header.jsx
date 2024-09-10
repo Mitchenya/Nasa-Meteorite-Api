2564641c9d1e4591b0eb524ff486d20e01af576a
Mitchenya <Mitchellrjshepherd@outlook.com>
2024-09-10 16:35:34 +0100

    This commit will add a Netlify file

--- src/header.jsx
+++ src/header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     <div className="header">
       <h1>Destinations Impacted by Meteorites Over The Years.</h1>
       <div className="logo">
-        <img className="logo-size" src="meteor_logo_white.png" />
+        <img className="logo-size" src="src/assets/meteor_logo_white.png" />
       </div>
     </div>
   );
